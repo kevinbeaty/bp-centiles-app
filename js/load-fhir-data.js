1cc27f8083d3083a664229f230700f5a3ceabb18
Stefan Baumgartner <stefanbaumg@gmail.com>
2016-10-01 21:20:27 -0400

    Highlighting a record in table view if it is the current record

--- js/load-fhir-data.js
+++ js/load-fhir-data.js
@@ -50,7 +50,7 @@
     */
     return dfd.promise();
   }
-  
+
 
   function cachedLink(items, target) {
     var match = null;
@@ -73,7 +73,7 @@
       vitals.heightData.push({
         vital_date: v.effectiveDateTime,
         height: smart.units.cm(v.valueQuantity)
-      }); 
+      });
     });
 
     (vitalsByCode['55284-4']||[]).forEach(function(v){
@@ -90,15 +90,17 @@
       		return coding.code === "8480-6";
       	});
       });
+
       var systolic = systolicObs.valueQuantity.value;
       var diastolic = diastolicObs.valueQuantity.value;
       var extensions = v.extension;
       var obj = {
         vital_date: v.effectiveDateTime,
         systolic: systolic,
-        diastolic: diastolic
+        diastolic: diastolic,
+        isCurrent: v.meta && v.meta.isCurrent
       };
-      
+
       if (extensions) {
          var position = extensions.find(function(extension) {
             return extension.url === "http://fhir-registry.smarthealthit.org/StructureDefinition/vital-signs#position";
@@ -108,7 +110,7 @@
              obj["bodyPositionCode"] = coding.system + coding.code;
          }
       }
-      
+
       if (v.encounter){
            var encounter = cachedLink(encounters, v.encounter);
            var encounter_type = encounter.class;
@@ -117,7 +119,7 @@
            }
            obj["encounterTypeCode"] = "http://smarthealthit.org/terms/codes/EncounterType#" + encounter_type;
       }
-              
+
       if (v.bodySite) {
         obj["bodySiteCode"] = v.bodySite.coding[0].system + v.bodySite.coding[0].code;
       }
@@ -125,19 +127,16 @@
       if (v.method) {
         obj["methodCode"] = v.method.coding[0].system + v.method.coding[0].code;
       }
-      
-      //obj["encounterTypeCode"] = "http://smarthealthit.org/terms/codes/EncounterType#ambulatory";
-      
       vitals.bpData.push(obj);
     });
 
     return vitals;
   };
 
-  function getObservations(){
+function getObservations(){
     return getFixtureJSON('observations')
         //return smart.patient.api.fetchAll({type: "Observation", query: {code: {$or: ['http://loinc.org|8302-2','http://loinc.org|55284-4']}}});
-        
+
   };
 
   function defaultOnFail(promise, defaultValue) {
@@ -163,7 +162,7 @@
     })
     return result
   }
-  
+
   function getEncounters(){
     return getFixtureJSON('encounters')
     //  return defaultOnFail(smart.patient.api.fetchAll({type: "Encounter"}),[]);
@@ -181,6 +180,11 @@
 						ret[coding.code] = ret[coding.code] || [];
 						ret[coding.code].push(o);
 					});
+
+          // set meta property if present
+          if (o.hasOwnProperty('meta') && o.meta.isCurrent){
+            ret.isCurrent = o.meta.isCurrent;
+          }
 			}
 		});
 		return ret;
